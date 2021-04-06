3445716f620840abfd90a81c3e1d5b20855fb0a5
Axel <anamujimbo@gmail.com>
2021-04-06 17:44:12 +0200

    countries ajax

--- api-countries/callAPI.js
+++ api-countries/callAPI.js
@@ -1,6 +1,9 @@
 var request = require("request");
 
-request.get("/countries", function (err, res, body) {
+request.get("http://localhost:8000/countries", function (err, res, body) {
 
+        var countries = JSON.parse(data)
+
+        console.log(countries.reverse());
 
 })
