57025304d1fae873d51e4652d88a65738bef2fc5
Axel <anamujimbo@gmail.com>
2021-04-29 17:30:14 +0200

    Back Server

---
+++ api-countries-react/dataCountries.js
@@ -0,0 +1,23 @@
+const countries = [
+    {
+        // country: "france",
+        // capital: "paris",
+       
+    },
+    {
+        country: "brazil",
+        capital: "brasilia",
+    },
+    {
+        country: "croatia",
+        capital: "zagreb",
+
+
+        country: {
+            ctr: {
+                name: "france",
+                capitale: "paris"
+            },
+            
+    },
+    }]
---
+++ api-countries-react/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const cors = require("cors")
+const { country, teams } = require("./dataCountries")
+
+app.get("/country/", (req, res) => {
+
+    
+})
+
+app.listen(port, () => {
+    console.log("Server à l'écoute dans le port 8000");
+})
