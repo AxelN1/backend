d7fa92a782a0e94a6994db582d749f8b1bef4c13
Axel <anamujimbo@gmail.com>
2021-04-30 12:31:16 +0200

    Back Server

--- api-countries-react/server.js
+++ api-countries-react/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors")
-const { country, teams } = require("./dataCountries")
+const { countries } = require("./dataCountries")
 
 app.get("/country/", (req, res) => {
 
