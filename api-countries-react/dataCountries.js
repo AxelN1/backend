562931901ee79c7ec387b5d78d08cb06ddfd7f5e
Axel <anamujimbo@gmail.com>
2021-04-30 12:30:26 +0200

    Back Server

--- api-countries-react/dataCountries.js
+++ api-countries-react/dataCountries.js
@@ -1,23 +1,62 @@
-const countries = [
-    {
-        // country: "france",
-        // capital: "paris",
-       
-    },
-    {
-        country: "brazil",
-        capital: "brasilia",
-    },
-    {
-        country: "croatia",
-        capital: "zagreb",
 
-
-        country: {
-            ctr: {
-                name: "france",
-                capitale: "paris"
-            },
-            
-    },
-    }]
+    const countries = [
+        {
+            name: "France",
+            capital: "Paris",
+            region: "Europe",
+            subregion: "Western Europe",
+            population: 66710000,
+            latlng: [
+                46,
+                2
+            ],
+            currencies: [
+                {
+                    code: "EUR",
+                    name: "Euro",
+                    symbol: "€"
+                }
+            ],
+            flag: "https://restcountries.eu/data/fra.svg"
+        },
+        {
+            name: "Brazil",
+            capital: "Brasilia",
+            region: "Americas",
+            subregion: "South America",
+            population: 206135893,
+            latlng: [
+                -10,
+                -55
+            ],
+            currencies: [
+                {
+                    code: "BRL",
+                    name: "Brazilian real",
+                    symbol: "R$"
+                }
+            ],
+            flag: "https://restcountries.eu/data/bra.svg"
+        },
+        {
+            name: "Croatia",
+            capital: "Zagreb",
+            region: "Europe",
+            subregion: "Southern Europe",
+            population: 4190669,
+            latlng: [
+                45.16666666,
+                15.5
+            ],
+            currencies: [
+                {
+                    code: "HRK",
+                    name: "Croatian kuna",
+                    symbol: "kn"
+                }
+            ],
+            flag: "https://restcountries.eu/data/hrv.svg"
+        }
+    ]
+    
+    module.exports = { countries }
