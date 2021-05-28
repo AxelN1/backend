35afbee281ed1715054ab0d14f29bc073b71c479
Axel <anamujimbo@gmail.com>
2021-05-28 17:18:37 +0200

    server_superHeros

--- chapitre_02/heros/server.js
+++ chapitre_02/heros/server.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 
 const app = express()
 
-const herosName = require("./herosData")
+const herosName = require("./superHeros")
 
 app.use(express.json()) 
 app.use(cors())
@@ -21,3 +21,13 @@ app.get("/heros", (req, res, next) => {
     console.log(body);
     next();
   });
+
+  app.get('*', (req, res) => {
+    res.json({
+        errorMessage: "Erreur de chemin"
+    })
+})
+
+  app.listen(port, () => {
+    console.log("Server à l'écoute dans le port " + port);
+})
