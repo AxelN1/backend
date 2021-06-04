b93093fd1e9678dc84d076215f77678c34605b35
Axel <anamujimbo@gmail.com>
2021-06-04 17:30:36 +0200

    'SQL'

--- Database/school.js
+++ Database/school.js
@@ -82,19 +82,70 @@ db.students.find()
 use languages
 
 db.languages.insert(
-{Language: "French"}
+  { Language: "French" }
 )
 db.languages.insert(
-{Language: "English"}
+  { Language: "English" }
 )
 db.languages.insert(
-{Language: "German"}
+  { Language: "German" }
 )
 db.languages.insert(
-{Language: "Spanish"}
+  { Language: "Spanish" }
 )
 db.languages.insert(
-{Language: "Mandarin"}
+  { Language: "Mandarin" }
 )
 
 db.languages.find()
+
+
+
+// FAVORITES
+
+use favorites
+
+db.favorites.insert(
+  {
+    class: "Maths",
+    sport: "Cricket",
+    student_id: 4
+  }
+)
+db.favorites.insert(
+  {
+    class: "Music",
+    sport: "Hip-Hop",
+    student_id: 2
+  }
+)
+db.favorites.insert(
+  {
+    class: "Arts",
+    sport: "Boxing",
+    student_id: 6
+  }
+)
+db.favorites.insert(
+  {
+    class: "Litterature",
+    sport: "Tennis",
+    student_id: 1
+  }
+)
+db.favorites.insert(
+  {
+    class: "Computer science",
+    sport: "Tennis",
+    student_id: 3
+  }
+)
+db.favorites.insert(
+  {
+    class: "Arts",
+    sport: "Baseball",
+    student_id: 5
+  }
+)
+
+db.favorites.find()
