9096b4c8a6d6d7a762f9f4afc0e8c124e374d690
Axel <anamujimbo@gmail.com>
2021-06-08 17:31:10 +0200

    garage-mongoose

--- Database/garage/db.js
+++ Database/garage/db.js
@@ -1,42 +1,100 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/garage')
+const mongoose = require('mongoose')
 
-console.log("Connexion effectuée avec succès !");
-
-// Création de l'entité cars
-const cars = new mongoose.Schema({
-    brand: String,
-    model: String,
-    year: Number,
-    created: {
-        type: Date,
-        defaut: Date.now
+mongoose.connect("mongodb://localhost:27017/garage", (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("I'm connected to the database")
     }
 })
 
-// Création d'un modèle mongoose de Car
-const Car = mongoose.model('Car', cars)
-
-
-// Ajout de voiture
-const renault = new Car({
-    brand: "Renault",
-    model: "Espace",
-    year: 1999
+const carSchema = new mongoose.Schema({
+    brand: String,
+    model: String,
+    year: Number,
+    created: { type: Date, default: Date.now }
 })
-renault.save().then(res => console.log(res))
 
+const Car = mongoose.model("Car", carSchema)
 
-const renault2 = new Car({
-    brand: "Renault",
-    model: "Scenic",
-    year: 2004
+Car.insertMany([
+    {
+        brand: "Renault",
+        model: "Espace",
+        year: "1999"
+    },
+    {
+        brand: "Renault",
+        model: "Scenic",
+        year: "2004"
+    },
+    {
+        brand: "Peugeot",
+        model: "308",
+        year: "2017"
+    },
+]).then(data => {
+    console.log(data);
+}).catch(err => {
+    console.error("Error insertMany Card: ", err);
 })
-renault2.save().then(res => console.log(res))
 
-const peugeot = new Car({
-    brand: "Peugeot",
-    model: "308",
-    year: 2017
-})
-peugeot.save().then(res => console.log(res))
+const findCarById = async () => {
+
+    try {
+        // const car = await Car.findOne({ _id: "60bf6ba05af23a21ebedc0ac" })
+        const car = await Car.findById("60bf6ba05af23a21ebedc0ac")
+
+        console.log("findCar", car)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+// findCarById()
+
+const updateCarByID = async (newValues) => {
+
+    try {
+        const car =  await Car.findByIdAndUpdate("60bf6ba05af23a21ebedc0ac", newValues)
+        
+        console.log("updateCarByID", car);
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+// updateCarByID({
+//     year: 2001
+// })
+
+const updateCar = async (newValues) => {
+
+    try {
+        const car =  await Car.findById("60bf6ba05af23a21ebedc0ac")
+
+        car.model = "Espace 2"
+
+        await car.save()
+        
+        console.log("updateCar", car);
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+// updateCar()
+
+const deleteManyCars = async () => {
+
+    try {
+
+        await Car.deleteMany({ brand: "Renault" })
+
+    } catch (err) {
+        console.error(err)
+    }
+
+}
+
+// deleteManyCars()
