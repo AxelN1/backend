afbb7bdbbb1aba651011beae97c44eb9618c4124
Axel <anamujimbo@gmail.com>
2021-06-18 16:24:42 +0200

    trippy controllers

--- trippy_basics_api/controllers/hotelControllers.js
+++ trippy_basics_api/controllers/hotelControllers.js
@@ -1,47 +1,54 @@
-const hotelModel = require("../models/hotel")
+const hotelModel = require("../models/hotel");
 const expressValidator = require("express-validator");
 
 const getHotels = async (req, res) => {
     try {
-        const hotels = await hotelModel.find().lean()
+        const hotels = await hotelModel.find().lean();
 
-        res.json(hotels)
+        res.json(hotels);
     } catch (error) {
-
-        res.status(500).json({ message: "There was a problem", error })
+        res.status(500).json({ message: "There was a problem", error });
     }
-}
+};
 
 const getHotel = async (req, res) => {
     try {
-        const idHotel = req.params.id
-        const hotel = await hotelModel.findById(idHotel).lean()
+        const idHotel = req.params.id;
+        const hotel = await hotelModel.findById(idHotel).lean();
 
-        res.json(hotel)
+        res.json(hotel);
     } catch (error) {
-
-        res.status(500).json({ message: "There was a problem", error })
+        res.status(500).json({ message: "There was a problem", error });
     }
-}
+};
 
 const addHotel = async (req, res) => {
     try {
         const errors = expressValidator.validationResult(req);
 
         if (!errors.isEmpty()) {
-            res.status(400).json({ errors: errors.array() })
+            res.status(400).json({ errors: errors.array() });
         } else {
-            const newHotel = await hotelModel.create(req.body)
+            const newHotel = await hotelModel.create(req.body);
 
-            res.json({ message: "The hotel was added!", newHotel })
+            res.json({ message: "The hotel was added!", newHotel });
         }
     } catch (error) {
-        res.status(500).json({ message: "There was a problem", error })
+        res.status(500).json({ message: "There was a problem", error });
     }
-}
+};
+
+const updateHotelName = async (req, res) => {
+    try {
+        res.json();
+    } catch (error) {
+        res.status(500).json({ message: "There was a problem", error });
+    }
+};
 
 module.exports = {
     getHotels,
     getHotel,
-    addHotel
-}
+    addHotel,
+    updateHotelName,
+};
